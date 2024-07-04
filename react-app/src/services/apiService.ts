1943b572284a37528027cbc283d7c03d590ab835
Sharon Haim-Pour <hpsharon@gmail.com>
2024-07-04 12:07:32 +0300

    heroku api support

--- react-app/src/services/apiService.ts
+++ react-app/src/services/apiService.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:4000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
 
 export const fetchMachines = async () => {
   const response = await fetch(`${API_URL}/machines`);
